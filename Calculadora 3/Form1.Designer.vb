c9e0ee139eefb64e4092b4699bf6c2abbcc2641a
Jordi Martínez Calante <jordimartinezcalante@outlook.com>
2015-10-02 18:52:11 +0200

    first commit

---
+++ Calculadora 3/Form1.Designer.vb
@@ -0,0 +1,152 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Sumabtn = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RestarBtn = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Sumabtn
+        '
+        Me.Sumabtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Sumabtn.ForeColor = System.Drawing.Color.Yellow
+        Me.Sumabtn.Location = New System.Drawing.Point(38, 106)
+        Me.Sumabtn.Name = "Sumabtn"
+        Me.Sumabtn.Size = New System.Drawing.Size(75, 23)
+        Me.Sumabtn.TabIndex = 0
+        Me.Sumabtn.Text = "Sumar"
+        Me.Sumabtn.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.MenuText
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.ForeColor = System.Drawing.Color.Yellow
+        Me.TextBox1.Location = New System.Drawing.Point(134, 106)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(55, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.MenuText
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox2.ForeColor = System.Drawing.Color.Yellow
+        Me.TextBox2.Location = New System.Drawing.Point(134, 139)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(55, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Yellow
+        Me.Label1.Location = New System.Drawing.Point(97, 58)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(33, 31)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "   "
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'RestarBtn
+        '
+        Me.RestarBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.RestarBtn.ForeColor = System.Drawing.Color.Yellow
+        Me.RestarBtn.Location = New System.Drawing.Point(38, 137)
+        Me.RestarBtn.Name = "RestarBtn"
+        Me.RestarBtn.Size = New System.Drawing.Size(75, 23)
+        Me.RestarBtn.TabIndex = 4
+        Me.RestarBtn.Text = "Restar"
+        Me.RestarBtn.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Yellow
+        Me.Label2.Location = New System.Drawing.Point(52, 19)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(127, 25)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Calculadora"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.Yellow
+        Me.Label3.Location = New System.Drawing.Point(12, 183)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(63, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Versión: 3.0"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.ForeColor = System.Drawing.Color.Yellow
+        Me.Label4.Location = New System.Drawing.Point(104, 183)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(112, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Creado por: Luli MaCa"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(228, 205)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.RestarBtn)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Sumabtn)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Calculadora"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Sumabtn As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RestarBtn As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Calculadora 3/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    'op1 es operando1
+    Dim op1 As Integer = 0
+    Dim op2 As Integer = 0
+    Dim resultado As Integer = 0
+
+    Private Sub Sumabtn_Click(sender As Object, e As EventArgs) Handles Sumabtn.Click
+        op1 = CInt(TextBox1.Text)
+        op2 = CInt(TextBox2.Text)
+
+        resultado = op1 + op2
+
+        Label1.Text = resultado.ToString()
+    End Sub
+
+    Private Sub RestarBtn_Click(sender As Object, e As EventArgs) Handles RestarBtn.Click
+        op1 = CInt(TextBox1.Text)
+        op2 = CInt(TextBox2.Text)
+
+        resultado = op1 - op2
+
+        Label1.Text = resultado.ToString()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
